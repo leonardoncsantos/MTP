ece292d530de80378fc7c581270f4ed334fd5f6b
leonardoncsantos <31476196+leonardoncsantos@users.noreply.github.com>
2017-12-13 11:13:27 -0200

    Update dec1.c

--- aula20170906/dec1.c
+++ aula20170906/dec1.c
@@ -3,3 +3,21 @@
 
 { 
 int main 
+  int numero;
+  printf("digite um numero natural: ");
+  scanf( "%d", &numero);
+  if(numero%2==0)
+  {
+    if(numero%3==0)
+      printf("o numero é multiplo de 3");
+      
+    if(numero%7==0)  
+      printf("o numero é multiplo de 7");
+  }
+  else
+  {
+    if(numero%5==0)
+      printf("o numero é multiplo de 5");
+  }
+  return 0;
+}
