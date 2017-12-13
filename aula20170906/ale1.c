2cf73b1996b96a089182bd49e5282755d5a37b95
leonardoncsantos <31476196+leonardoncsantos@users.noreply.github.com>
2017-12-13 11:21:58 -0200

    Update ale1.c

--- aula20170906/ale1.c
+++ aula20170906/ale1.c
@@ -2,9 +2,11 @@
 #include <time.h>
 int main ()
 {
-     float ladoquadrado, areaquadrado;
-     printf("entre com o lado do quadrado: ");
-     scanf("%f",&ladoquadrado);
-     areaquadrado= ladoquadrado*ladoquadrado;
-     print(" area do quadrado: %.3f\n",areaquadrado);
+     
+     int x =rand()%(0-100);
+     int y= rand()%(101-293+1);
+     printf("o valor do numero pseudo aleatorio entre 0 e 100 eh: %d", x);
+     printf("o valor do numero pseudo aleatorio entre 101 e 293 eh: %d",y);
      return 0;
+     
+}
